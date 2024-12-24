6a31eb168637d236c6a3ef87bc6173bcc581c728
C0B23095 <c0b23095c0@edu.teu.ac.jp>
2024-12-24 14:49:58 +0900

    ステージ作成マージ完了

--- Gravity_koukaton.py
+++ Gravity_koukaton.py
@@ -30,7 +30,7 @@ class Gravity:
     """
     重力とジャンプを管理するクラス
     """
-    def __init__(self, gravity: float = 0.5, jump_power: float = -10):
+    def __init__(self, gravity: float = 0.5, jump_power: float = -20):
         """
         初期化
         引数:gravity: 下方向の加速度
@@ -300,7 +300,7 @@ class Enemy:
         りんごの読み込みと初期速度
         """
         self.vy = speed
-        self.img = pg.transform.rotozoom(pg.image.load(f"ringo.png"), 0, 0.05)
+        self.img = pg.transform.rotozoom(pg.image.load(f"fig/ringo.png"), 0, 0.05)
         self.rct = self.img.get_rect()
         self.rct.center = (x, y)
         self.rct.top = y
@@ -322,7 +322,7 @@ class Enemy:
 
 class ClearObj:
     def __init__(self, x:int, y:int):
-        self.img = pg.transform.rotozoom(pg.image.load(f"glayringo.png"), 0, 0.1)
+        self.img = pg.transform.rotozoom(pg.image.load(f"fig/glayringo.png"), 0, 0.1)
         self.rct = self.img.get_rect()
         self.rct.right = x
         self.rct.bottom = y
@@ -350,11 +350,11 @@ def main():
         Enemy(400, 522, 2)
     ]
     enemies2 = [
-        Enemy(700, 15, -4),
-        Enemy(700, 138, -4),
-        Enemy(700, 266, -4),
-        Enemy(700, 394, -4),
-        Enemy(700, 522, -4)
+        Enemy(700, 15, -2),
+        Enemy(700, 138, -2),
+        Enemy(700, 266, -2),
+        Enemy(700, 394, -2),
+        Enemy(700, 522, -2)
     ]
 
     #explosionリスト初期化
@@ -371,6 +371,7 @@ def main():
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 #ジャンプの処理
                 if bird.gravity_maneger.flag == False:  # 地面にいるときだけジャンプ
+                    bird.vy = bird.gravity_maneger.jump()
                     bird.gravity_maneger.flag = True
 
         if stage.hit_stage(bird):
